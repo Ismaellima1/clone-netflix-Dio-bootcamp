62d70dc9d2f1104f509d09e70f8211f632a6ba62
ismael lima <ismaellimadeolivera2004@gmail.com>
2022-07-20 19:01:50 -0300

    botão voltar do player do trailer bug fix

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 function botaoAssistir(){
   document.querySelector(".video").style.display = "block";
   document.querySelector(".info").style.display = "none";
+  document.querySelector(".filme-principal").style.flexDirection = "row";
+  document.querySelector("#bnt-voltarInfo").style.display = "block";
 }
 function voltarInfo(){
   document.querySelector(".video").style.display = "none";
   document.querySelector(".info").style.display = "block";
+  document.querySelector(".filme-principal").style.flexDirection = "column";
+  document.querySelector("#bnt-voltarInfo").style.display = "none";
 }
 
 function botaoSobre(){
